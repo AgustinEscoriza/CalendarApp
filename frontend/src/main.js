17e8c3f1fe5c1b2922f374e18df343d6370a861f
AgustinEscoriza <agusescoriza@outlook.es>
2025-06-02 21:21:26 -0300

    naiveUI + custom css global config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import i18n from './i18n'
+import naive from './plugins/naive-ui'
 import './style.css'
+import './styles/fullcalendar-theme.css'
 import App from './App.vue'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
+app.use(naive)
 app.mount('#app')
